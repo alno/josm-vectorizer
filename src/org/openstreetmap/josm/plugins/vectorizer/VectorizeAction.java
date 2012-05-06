02f29c54034a40629c8ee6ccff01e72cb9048a8d
Alexey Noskov <alexey.noskov@gmail.com>
2012-05-06 12:05:29 +0400

    Select visible layer

--- src/org/openstreetmap/josm/plugins/vectorizer/VectorizeAction.java
+++ src/org/openstreetmap/josm/plugins/vectorizer/VectorizeAction.java
@@ -8,13 +8,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.gui.layer.ImageryLayer;
+import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Shortcut;
 
@@ -22,7 +22,7 @@ public class VectorizeAction extends JosmAction {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final Shortcut shortcut = Shortcut.registerShortcut( "tools:vectorize", tr( "Tool: {0}", tr( "Vectorizer" ) ), KeyEvent.VK_V, Shortcut.SHIFT );
+	private static final Shortcut shortcut = Shortcut.registerShortcut( "tools:vectorize", tr( "Tool: {0}", tr( "Vectorizer" ) ), KeyEvent.VK_V, Shortcut.DIRECT );
 
 	private final MouseListener mouseListener = new MouseAdapter() {
 
@@ -35,18 +35,27 @@ public class VectorizeAction extends JosmAction {
 				oldCursor = null;
 				active = false;
 
-				List<ImageryLayer> imageryLayers = Main.map.mapView.getLayersOfType( ImageryLayer.class );
+				ImageryLayer layer = selectImageryLayer();
 
-				if ( imageryLayers.isEmpty() ) {
+				if ( layer == null ) {
 					JOptionPane.showMessageDialog( null, tr( "No imagery layers found!" ) );
 					return;
 				}
 
-				new Thread( new VectorizeTask( tr( "Vectorizing" ), imageryLayers.get( 0 ), e.getPoint() ) ).start();
+				new Thread( new VectorizeTask( tr( "Vectorizing" ), layer, e.getPoint() ) ).start();
 			}
 		}
 
+		private ImageryLayer selectImageryLayer() {
+			for ( Layer layer : Main.map.mapView.getAllLayers() )
+				if ( layer.isVisible() && layer instanceof ImageryLayer )
+					return (ImageryLayer) layer;
+
+			return null;
+		}
+
 	};
+
 	private boolean active = false;
 	private Cursor oldCursor = null;
 
